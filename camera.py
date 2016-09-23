54a2d2d617c762840792e7b5cddcf2918bb20edb
Mick Phillips <mick.phillips@gmail.com>
2016-09-23 17:18:38 +0100

    Defined more of the camera interface.
    Signed-off-by: Mick Phillips <mick.phillips@gmail.com>

--- camera.py
+++ camera.py
@@ -19,12 +19,14 @@ import abc
 import devicebase
 import Pyro4
 
+# Triggering types.
+(TRIGGER_AFTER, TRIGGER_BEFORE, TRIGGER_DURATION) = range(3)
+
 class CameraDevice(devicebase.DataDevice):
-    __metaclass_ = abc.ABCMeta
     """Adds functionality to DataDevice to support cameras.
 
-    Applies a transform to acquired data in the processing step.
     Defines the interface for cameras.
+    Applies a transform to acquired data in the processing step.
     """
     def __init__(self, *args, **kwargs):
         # A tuple defining data shape.
@@ -53,13 +55,70 @@ class CameraDevice(devicebase.DataDevice):
 
     @abc.abstractmethod
     @Pyro4.expose
+    def set_exposure_time(self, value):
+        pass
+
+
     def get_exposure_time(self):
         pass
 
 
-    def set_some_setting(self, value):
-        self.some_setting = value
+    def get_cycle_time(self):
+        pass
 
 
-    def get_some_setting(self, value):
-        return self.some_setting
+    def get_sensor_shape(self):
+        """Return a tuple of (width, height)."""
+        pass
+
+
+    def get_binning(self):
+        """Return a tuple of (horizontal, vertical)."""
+        pass
+
+
+    def set_binning(self, h_bin, v_bin):
+        """Set binning along both axes."""
+        pass
+
+
+    def get_sensor_temperature(self):
+        """Return the sensor temperature."""
+        pass
+
+
+    def get_roi(self):
+        """Return ROI as a rectangle (x0, y0, width, height).
+
+        Chosen this rectangle format as it completely defines the ROI without
+        reference to the sensor geometry."""
+        pass
+
+
+    def set_roi(self, x, y, width, height):
+        """Set the ROI according to the provided rectangle."""
+        pass
+
+
+    def get_gain(self):
+        """Get the current amplifier gain."""
+        pass
+
+
+    def set_gain(self):
+        """Set the amplifier gain."""
+        pass
+
+
+    def get_trigger_type(self):
+        """Return the current trigger mode.
+
+        One of
+            TRIGGER_AFTER,
+            TRIGGER_BEFORE or
+            TRIGGER_DURATION (bulb exposure.)
+        """
+
+    def get_meta_data(self):
+        """Return metadata."""
+        pass
