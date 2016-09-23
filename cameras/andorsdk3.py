623c37579dcedae6aca907da3632695647ee45ee
Mick Phillips <mick.phillips@gmail.com>
2016-09-23 17:24:34 +0100

    Settings working over Pyro for andorsdk3.

--- cameras/andorsdk3.py
+++ cameras/andorsdk3.py
@@ -15,19 +15,149 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""AndorSDK3 camera device.
+
+This class provides a wrapper for PYME's SDK3 interface that allows
+a camera and all its settings to be exposed over Pyro.
+"""
 import abc
 import camera
 import devicebase
 from PYME.Acquire.Hardware.AndorNeo.SDK3Cam import *
 
 
+# Wrapper to ensure feature is readable.
+def readable_wrapper(func):
+    def wrapper(self, *args, **kwargs):
+        if SDK3.IsReadable(self.handle, self.propertyName):
+            return func(self, *args, **kwargs)
+        else:
+            return None#Warning('%s not currently readable.' % self.propertyName)
+    return wrapper
+
+# Wrapper to ensure feature is writable.
+def writable_wrapper(func):
+    def wrapper(self, *args, **kwargs):
+        if SDK3.IsWritable(self.handle, self.propertyName):
+            return func(self, *args, **kwargs)
+        else:
+            return False#Warning('%s not currently writable.' % self.propertyName)
+    return wrapper
+
+
+
+# Overrides for local style and error handling.
+ATInt.get_value = readable_wrapper(ATInt.getValue)
+ATInt.set_value = writable_wrapper(ATInt.setValue)
+ATInt.min = readable_wrapper(ATInt.min)
+ATInt.max = readable_wrapper(ATInt.max)
+ATBool.get_value = readable_wrapper(ATBool.getValue)
+ATBool.set_value = writable_wrapper(ATBool.setValue)
+ATFloat.get_value = readable_wrapper(ATFloat.getValue)
+ATFloat.set_value = writable_wrapper(ATFloat.setValue)
+ATString.get_value = readable_wrapper(ATString.getValue)
+ATString.set_value = writable_wrapper(ATString.setValue)
+ATString.max_length = readable_wrapper(ATString.maxLength)
+ATEnum.get_index  = readable_wrapper(ATEnum.getIndex)
+ATEnum.set_index  = writable_wrapper(ATEnum.setIndex)
+ATEnum.get_string = readable_wrapper(ATEnum.getString)
+ATEnum.set_string = writable_wrapper(ATEnum.setString)
+ATEnum.get_available_values = readable_wrapper(ATEnum.getAvailableValues)
+
+# Mapping of AT type to python type.
+PROPERTY_TYPES = {
+    ATInt: int,
+    ATBool: bool,
+    ATFloat: float,
+    ATString: str,
+    ATEnum: 'enum'
+}
+
+
 class AndorSDK3(camera.CameraDevice,
-                devicebase.FloatingDeviceMixin):
+                devicebase.FloatingDeviceMixin,
+                SDK3Camera):
     SDK_INITIALIZED = False
     def __init__(self, *args, **kwargs):
         super(AndorSDK3, self).__init__(**kwargs)
         if not AndorSDK3.SDK_INITIALIZED:
             SDK3.InitialiseLibrary()
+        self._index = kwargs.get('index', 0)
+        self._handle = None
+        SDK3Camera.__init__(self, self._index)
+
+        # Define features with local style. The SDK treats parameter names
+        # without regard to case, so we just need to remove the underscores
+        # when connecting properties to SDK calls. We define all possible
+        # features here; they will be removed if they are not implemented
+        # on the camera.
+        self._accumulate_count = ATInt()
+        self._acquisition_start = ATCommand()
+        self._acquisition_stop = ATCommand()
+        self._aoi_binning = ATEnum()
+        self._aoi_height = ATInt()
+        self._aoi_left = ATInt()
+        self._aoi_top = ATInt()
+        self._aoi_width = ATInt()
+        self._auxiliary_out_source = ATEnum()
+        self._baseline_level = ATInt()
+        self._bit_depth = ATEnum()
+        self._buffer_overflow_event = ATInt()
+        self._bytes_per_pixel = ATFloat()
+        self._camera_acquiring = ATBool()
+        self._camera_dump = ATCommand()
+        self._camera_model = ATString()
+        self._camera_name = ATString()
+        self._camera_present = ATBool()
+        self._controller_id = ATString()
+        self._frame_count = ATInt()
+        self._cycle_mode = ATEnum()
+        self._electronic_shuttering_mode = ATEnum()
+        self._event_enable = ATBool()
+        self._events_missed_event = ATInt()
+        self._event_selector = ATEnum()
+        self._exposure_time = ATFloat()
+        self._exposure_end_event = ATInt()
+        self._exposure_start_event = ATInt()
+        self._fan_speed = ATEnum()
+        self._firmware_version = ATString()
+        self._frame_rate = ATFloat()
+        self._full_aoi_control = ATBool()
+        self._image_size_bytes = ATInt()
+        self._interface_type = ATString()
+        self._io_invert = ATBool()
+        self._io_selector = ATEnum()
+        self._lut_index = ATInt()
+        self._lut_value = ATInt()
+        self._max_interface_transfer_rate = ATFloat()
+        self._metadata_enable = ATBool()
+        self._metadata_timestamp = ATBool()
+        self._metadata_frame = ATBool()
+        self._overlap = ATBool()
+        self._pixel_encoding = ATEnum()
+        self._pixel_readout_rate = ATEnum()
+        self._pre_amp_gain_control = ATEnum()
+        self._readout_time = ATFloat()
+        self._rolling_shutter_global_clear = ATBool()
+        self._row_n_exposure_end_event = ATInt()
+        self._row_n_exposure_start_event = ATInt()
+        self._sensor_cooling = ATBool()
+        self._sensor_height = ATInt()
+        self._sensor_temperature = ATFloat()
+        self._sensor_width = ATInt()
+        self._serial_number = ATString()
+        self._simple_pre_amp_gain_control = ATEnum()
+        self._software_trigger = ATCommand()
+        self._static_blemish_correction = ATBool()
+        self._spurious_noise_filter = ATBool()
+        self._target_sensor_temperature = ATFloat()
+        self._temperature_control = ATEnum()
+        self._temperature_status = ATEnum()
+        self._timestamp_clock = ATInt()
+        self._timestamp_clock_frequency = ATInt()
+        self._timestamp_clock_reset = ATCommand()
+        self._trigger_mode = ATEnum()
+        self._vertically_centre_aoi = ATBool()
 
 
     def _fetch_data(self):
@@ -35,23 +165,54 @@ class AndorSDK3(camera.CameraDevice,
 
 
     def abort(self):
-        pass
-
-
-    def enable(self):
-        pass
-
-
-    def disable(self):
-        pass
+        self._acquisition_stop()
 
 
     def initialize(self):
-        pass
+        """Initialise the camera.
+
+        Open the connection, connect properties and populate settings dict.
+        """
+        self.handle = SDK3.Open(self._index)
+        for name, var in self.__dict__.items():
+            sdk_name = name.replace('_', '')
+            if isinstance(var, ATProperty):
+                if not SDK3.IsImplemented(self.handle, sdk_name):
+                    delattr(self, name)
+                    continue
+                var.connect(self.handle, sdk_name)
+
+                if type(var) is ATCommand:
+                    continue
+                elif type(var) is ATEnum:
+                    set_func = var.set_index
+                    get_func = lambda v=var: (v.get_index(), v.get_string())
+                    vals_func = var.get_available_values
+                else:
+                    set_func = var.set_value
+                    get_func = var.get_value
+                    if type(var) is ATString:
+                        vals_func = var.max_length
+                    elif type(var) in (ATFloat, ATInt):
+                        vals_func = lambda v=var: (v.min(), v.max())
+                    else:
+                        vals_func = None
+                self.add_setting(name.lstrip('_'), PROPERTY_TYPES[type(var)],
+                                 get_func, set_func, vals_func)
+        self.set_cooling(True)
+
+
+    def set_cooling(self, value):
+        try:
+            self._sensor_cooling.set_value(value)
+        except AttributeError:
+            pass
+        except Exception:
+            raise
 
 
     def get_id(self):
-        pass
+        return self._serial_number.get_value()
 
 
     def make_safe(self):
@@ -59,13 +220,70 @@ class AndorSDK3(camera.CameraDevice,
 
 
     def shutdown(self):
+        self.set_cooling(False)
         SDK3.FinaliseLibrary()
-        pass
 
 
     def start_acquisition(self):
-        pass
+        self._acquisition_start()
+
+
+    def set_exposure_time(self, value):
+        self._exposure_time.set_value(value)
+        self._frame_rate.set_value(self._frame_rate.max())
 
 
     def get_exposure_time(self):
-        return 1.0      
+        return self._exposure_time.get_value()
+
+
+    def get_cycle_time(self):
+        return 1. / self._frame_rate.get_value()
+
+
+    def get_sensor_shape(self):
+        return (self._sensor_width.get_value(),
+                self._sensor_height.get_value())
+
+
+    def get_binning(self):
+         as_text = self._aoi_binning.get_string().split('x')
+         return tuple(int(t) for t in as_text)
+
+
+    def set_binning(self, h, v):
+        modes = self._aoi_binning.get_available_values()
+        as_text = '%dx%d' % (h,v)
+        if as_text in modes:
+            self._aoi_binning.set_string(as_text)
+            return True
+        else:
+            return False
+
+
+    def get_roi(self):
+        return (self._aoi_left.get_value(),
+                self._aoi_top.get_value(),
+                self._aoi_width.get_value(),
+                self._aoi_height.get_value())
+
+
+    def set_roi(self, x, y, width, height):
+        current = self.get_roi()
+        try:
+            self._aoi_left.set_value(x)
+            self._aoi_top.set_value(y)
+            self._aoi_width.set_value(width)
+            self._aoi_height.set_value(height)
+        except:
+            self._aoi_left.set_value(current[0])
+            self._aoi_top.set_value(current[1])
+            self._aoi_width.set_value(current[2])
+            self._aoi_height.set_value(current[3])
+            return False
+        return True
+
+
+    def get_gain(self):
+        if hasattr(self, '_preampgain'):
+            return sel
