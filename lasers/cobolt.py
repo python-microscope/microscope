e8c92af0a660489d8b9de856f1ee3fd970831056
Mick Phillips <mick.phillips@gmail.com>
2016-10-12 16:51:00 +0100

    Update cobolt to implement LaserDevice interface.

--- lasers/cobolt.py
+++ lasers/cobolt.py
@@ -18,11 +18,11 @@
 import serial
 import threading
 import time
-import laser
+import devices
 import functools
 
 
-def lockComms(func):
+def lock_comms(func):
     """A decorator to flush the input buffer prior to issuing a command.
 
     Locks the comms channel so that a function must finish all its comms
@@ -30,13 +30,13 @@ def lockComms(func):
     """
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
-        with self.commsLock:
+        with self.comms_lock:
             return func(self, *args, **kwargs)
 
     return wrapper
 
 
-class CoboltLaser(laser.LaserDevice):
+class CoboltLaser(devices.LaserDevice):
     def __init__(self, com=None, baud=None, timeout=0.01, **kwargs):
         super(CoboltLaser, self).__init__()
         self.connection = serial.Serial(port = com,
@@ -44,83 +44,61 @@ class CoboltLaser(laser.LaserDevice):
             stopbits = serial.STOPBITS_ONE,
             bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE)
         # Start a logger.
-        self.write('sn?')
-        response = self.readline()
-        self.logger.info("Cobolt laser serial number: [%s]" % response)
+        self._write('sn?')
+        response = self._readline()
+        self._logger.info("Cobolt laser serial number: [%s]" % response)
         # We need to ensure that autostart is disabled so that we can switch emission
         # on/off remotely.
-        self.write('@cobas 0')
-        self.logger.info("Response to @cobas 0 [%s]" % self.readline())
-        self.commsLock = threading.RLock()
+        self._write('@cobas 0')
+        self._logger.info("Response to @cobas 0 [%s]" % self._readline())
+        self.comms_lock = threading.RLock()
 
-
-    ## Simple passthrough.
-    def read(self, numChars):
-        return self.connection.read(numChars)
-
-
-    ## Simple passthrough.
-    def readline(self):
-        return self.connection.readline().strip()
-
-
-    ## Send a command. 
-    def write(self, command):
-        response = self.connection.write(command + '\r\n')
-        return response
-
-
-    ## Send command and retrieve response.
     def send(self, command):
-        self.write(str(command))
-        return self.readline()
+        """Send command and retrieve response."""
+        self._write(str(command))
+        return self._readline()
 
-
-    @lockComms
+    @lock_comms
     def clearFault(self):
-        self.write('cf')
-        self.readline()
-        return self.getStatus()
+        self._write('cf')
+        self._readline()
+        return self.get_status()
 
-
-    def flushBuffer(self):
+    def flush_buffer(self):
         line = ' '
         while len(line) > 0:
-            line = self.readline()
+            line = self._readline()
 
-    @lockComms
-    def isAlive(self):
-        self.write('l?')
-        response = self.readline()
+    @lock_comms
+    def is_alive(self):
+        self._write('l?')
+        response = self._readline()
         return response in '01'
 
-
-    @lockComms
-    def getStatus(self):
+    @lock_comms
+    def get_status(self):
         result = []
         for cmd, stat in [('l?', 'Emission on?'),
                             ('p?', 'Target power:'),
                             ('pa?', 'Measured power:'),
                             ('f?', 'Fault?'),
                             ('hrs?', 'Head operating hours:')]:
-            self.write(cmd)
-            result.append(stat + ' ' + self.readline())
+            self._write(cmd)
+            result.append(stat + ' ' + self._readline())
         return result
 
-
-    ## Things that should be done when cockpit exits.
-    @lockComms
-    def onExit(self):
+    @lock_comms
+    def _on_shutdown(self):
         # Disable laser.
         self.send('l0')
         self.send('@cob0')
-        self.flushBuffer()
+        self.flush_buffer()
 
 
     ##  Initialization to do when cockpit connects.
-    @lockComms
-    def onCockpitInitialize(self):
-        self.flushBuffer()
+    @lock_comms
+    def initialize(self):
+        self.flush_buffer()
         #We don't want 'direct control' mode.
         self.send('@cobasdr 0')
         # Force laser into autostart mode.
@@ -128,61 +106,61 @@ class CoboltLaser(laser.LaserDevice):
 
 
     ## Turn the laser ON. Return True if we succeeded, False otherwise.
-    @lockComms
+    @lock_comms
     def enable(self):
-        self.logger.info("Turning laser ON at %s" % time.strftime('%Y-%m-%d %H:%M:%S'))
+        self._logger.info("Turning laser ON.")
         # Turn on emission.
         response = self.send('l1')
-        self.logger.info("l1: [%s]" % response)
+        self._logger.info("l1: [%s]" % response)
 
-        if not self.getIsOn():
+        if not self.get_is_on():
             # Something went wrong.
-            self.logger.error("Failed to turn on. Current status:\r\n")
-            self.logger.error(self.getStatus())
+            self._logger.error("Failed to turn on. Current status:\r\n")
+            self._logger.error(self.get_status())
             return False
         return True
 
 
     ## Turn the laser OFF.
-    @lockComms
+    @lock_comms
     def disable(self):
-        self.logger.info("Turning laser OFF at %s" % time.strftime('%Y-%m-%d %H:%M:%S'))
-        self.write('l0')
-        return self.readline()
+        self._logger.info("Turning laser OFF.")
+        self._write('l0')
+        return self._readline()
 
 
     ## Return True if the laser is currently able to produce light.
-    @lockComms
-    def getIsOn(self):
-        self.write('l?')
-        response = self.readline()
+    @lock_comms
+    def get_is_on(self):
+        self._write('l?')
+        response = self._readline()
         return response == '1'
 
 
-    @lockComms
-    def getMaxPower_mW(self):
+    @lock_comms
+    def get_max_power_mw(self):
         # 'gmlp?' gets the maximum laser power in mW.
-        self.write('gmlp?')
-        response = self.readline()
+        self._write('gmlp?')
+        response = self._readline()
         return float(response)
 
 
-    @lockComms
-    def getPower_mW(self):
-        if not self.getIsOn():
+    @lock_comms
+    def get_power_mw(self):
+        if not self.get_is_on():
             return 0
-        self.write('pa?')
-        return 1000 * float(self.readline())
+        self._write('pa?')
+        return 1000 * float(self._readline())
 
 
-    @lockComms
-    def setPower_mW(self, mW):
-        mW = min(mW, self.getMaxPower_mW)
-        self.logger.info("Setting laser power to %.4fW at %s"  % (mW / 1000.0, time.strftime('%Y-%m-%d %H:%M:%S')))
+    @lock_comms
+    def _set_power_mw(self, mW):
+        mW = min(mW, self.get_max_power_mw)
+        self._logger.info("Setting laser power to %.4fW."  % (mW / 1000.0, ))
         return self.send("@cobasp %.4f" % (mW / 1000.0))
 
 
-    @lockComms
-    def getSetPower_mW(self):
-        self.write('p?')
-        return 1000 * float(self.readline())
+    @lock_comms
+    def get_set_power_mw(self):
+        self._write('p?')
+        return 1000 * float(self._readline())
