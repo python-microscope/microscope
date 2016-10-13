1236eeb60f6efc977be8a7129b72ec659432b4a3
Mick Phillips <mick.phillips@gmail.com>
2016-10-13 12:44:31 +0100

    Sample config updated.

--- config.py
+++ config.py
@@ -5,12 +5,10 @@ Import device classes, then define entries in DEVICES as:
    devices(CLASS, HOST, PORT, other_args)
 """
 # Function to create record for each device.
-from devicebase import device
+from devices import device
 # Import device modules/classes here.
-import devicebase
+from cameras import testcamera
 
 DEVICES = [
-	device(devicebase.Device, '127.0.0.1', 8000),
-	device(devicebase.Device, '127.0.0.1', 8001, com=6, baud=115200),
-	device(devicebase.FloatingDevice, '127.0.0.1', 8002, uid=None),
+	device(testcamera.TestCamera, '127.0.0.1', 8001, otherargs=1,),
 ]
