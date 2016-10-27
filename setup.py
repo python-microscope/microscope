990e4837d3095bcb9ffd09fedb4a2bae98ff61b3
David Pinto <david.pinto@bioch.ox.ac.uk>
2016-10-27 19:28:31 +0100

    Restructure repo for a python package for distribution with setuptools.
    * move all python modules into a microscope package.
    * add new __init__.py to microscope/ so it is identified as a package.
    * add new setup.py for package releases.
    * add a .gitignore file.

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## Copyright (C) 2016 David Pinto <david.pinto@bioch.ox.ac.uk>
+##
+## Copying and distribution of this file, with or without modification,
+## are permitted in any medium without royalty provided the copyright
+## notice and this notice are preserved.  This file is offered as-is,
+## without any warranty.
+
+import setuptools
+
+setuptools.setup(
+  name = "microscope",
+  version = "0.0.1",
+  description = "An extensible microscope hardware interface.",
+  license = "GPL-3.0+",
+
+  ## Do not use author_email because that won't play nice once there
+  ## are multiple authors.
+  author = "Mick Phillips <mick.phillips@bioch.ox.ac.uk>",
+
+  url = "https://github.com/MicronOxford/microscope",
+
+  packages = [
+    "microscope",
+    "microscope.cameras",
+    "microscope.lasers",
+  ],
+
+  install_requires = [
+    "PYME",
+    "numpy",
+    "Pyro4",
+    "pyserial",
+  ],
+
+  entry_points = {
+    "console_scripts" : [
+      "devices = microscope.devices",
+    ]
+  },
+
+  ## https://pypi.python.org/pypi?:action=list_classifiers
+  classifiers = [
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+  ],
+)
