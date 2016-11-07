b9c773459547e8079d8825681b40a9d40f68741a
Mick Phillips <mick.phillips@gmail.com>
2016-11-07 15:28:57 +0000

    Added testFilterWheel to config.

--- microscope/config.py
+++ microscope/config.py
@@ -8,7 +8,9 @@ Import device classes, then define entries in DEVICES as:
 from microscope.devices import device
 # Import device modules/classes here.
 from microscope.cameras import testcamera
+from microscope import testfilterwheel
 
 DEVICES = [
   device(testcamera.TestCamera, '127.0.0.1', 8001, otherargs=1,),
+  device(testfilterwheel.TestFilterwheel, '127.0.0.1', 8003),
 ]
