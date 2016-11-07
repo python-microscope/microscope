895401609f4b2212888046cda97bcc482278ddba
Mick Phillips <mick.phillips@gmail.com>
2016-11-07 17:33:30 +0000

    Test filter wheel.

---
+++ microscope/testfilterwheel.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+#
+# Copyright 2016 Mick Phillips (mick.phillips@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""A dummy filter wheel class. """
+from microscope import devices
+from future.utils import iteritems
+
+class TestFilterwheel(devices.Device):
+    def __init__(self, *args, **kwargs):
+        super(TestFilterwheel, self).__init__()
+        self._position = 0
+        self._filters = {0: 'GFP',
+                         1: 'Cy5',
+                         2: '4pass',
+                         3: None}
+        self._inv_filters = {val:key for key, val in iteritems(self._filters)}
+        # The position as an integer.
+        self.add_setting('position',
+                         'int',
+                         lambda: self._position,
+                         lambda val: setattr(self, '_position', val),
+                         (0,5))
+        # The selected filter.
+        self.add_setting('filter',
+                         'enum',
+                         lambda: self._filters[self._position],
+                         lambda val: setattr(self, '_position', self._inv_filters[val]),
+                         self._filters.values)
+
+    def initialize(self):
+        pass
+
+    def _on_shutdown(self):
+        pass
