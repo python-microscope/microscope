de9070eed8776a781aede63bf35269d620922583
Carnë Draug <carandraug+dev@gmail.com>
2019-12-18 12:26:50 +0000

    win32: explain what's going on when importing the win32* modules
    Mick reports that importing the win32* modules may fail because they
    import win32api which is a pyd file.  We catched this by trying to
    import win32api even though we were not actually making use of it but
    then code analysers complain about unusued import.
    
    This commit makes it so that we import what we actually use and
    documents why we are manipulating PATH (which was previously not clear
    anyway).

--- microscope/win32.py
+++ microscope/win32.py
@@ -4,17 +4,20 @@
 import os
 import sys
 
+# These win32* modules both import win32api which is a pyd file.
+# Importing win32api can be problematic because of Windows things
+# specially when running as a Windows.  So if it fails, add the
+# executable path to the DLL search PATH.
 try:
-    import win32api
+    import win32serviceutil
+    import win32service
 except:
-    # Add executable path to DLL search PATH.
     os.environ['PATH'] += ';' + os.path.split(sys.executable)[0]
-    import win32api
+    import win32serviceutil
+    import win32service
 
 import multiprocessing
 import servicemanager
-import win32serviceutil
-import win32service
 
 class MicroscopeWindowsService(win32serviceutil.ServiceFramework):
     """ Serves microscope devices via a Windows service.
