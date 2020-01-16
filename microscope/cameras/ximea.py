6c1974ab57f5b608614d39944b406d631287df28
Carnë Draug <carandraug+dev@gmail.com>
2020-01-16 18:01:25 +0000

    XimeaCamera: rewrite hardware trigger support via TrigerTargetMixIn (issue #131)
    Hardware trigger support was done by setting trigger source to edge
    rising and relying on the fact that on this state the camera would
    accept both hardware and software trigger.  The get_trigger_source,
    get_trigger_type, set_trigger_source, and set_trigger_type had never
    been used and were undocumented.
    
    This commit makes XimeaCamera subclass from TriggerTargetMixIn and
    implements its interface.  This also changes the default trigger type
    to software, and adds a new 'trigger source' so that trigger type can
    be set via the Settings mechanism.

--- microscope/cameras/ximea.py
+++ microscope/cameras/ximea.py
@@ -20,6 +20,20 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Microscope.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Ximea cameras.
+
+Camera Triggers
+---------------
+
+Changing the trigger type, named trigger source in the Ximea
+documentation, requires restarting image acquisition.  While this
+restart is done automatically, it will discard any images that are in
+the camera memory but have not yet been read.
+
+"""
+
+import contextlib
+import enum
 import logging
 import typing
 
@@ -40,6 +54,87 @@ _logger = logging.getLogger(__name__)
 xiapi.Camera().set_debug_level('XI_DL_ERROR')
 
 
+@contextlib.contextmanager
+def _disabled_camera(camera):
+    """Context manager to temporarily disable camera."""
+    if camera.enabled:
+        try:
+            camera.disable()
+            yield camera
+        finally:
+            camera.enable()
+    else:
+        yield camera
+
+@contextlib.contextmanager
+def _enabled_camera(camera):
+    """Context manager to temporarily enable camera."""
+    if not camera.enabled:
+        try:
+            camera.enable()
+            yield camera
+        finally:
+            camera.disable()
+    else:
+        yield camera
+
+
+@enum.unique
+class TrgSourceMap(enum.Enum):
+    # The complete list is the XI_TRG_SOURCE enum (C code) or in the
+    # xidefs module (Python code).
+
+    XI_TRG_SOFTWARE = devices.TriggerType.SOFTWARE
+    XI_TRG_EDGE_RISING = devices.TriggerType.RISING_EDGE
+    XI_TRG_EDGE_FALLING = devices.TriggerType.FALLING_EDGE
+
+    # Not all XI_TRG_SOURCE values are defined:
+    #
+    # XI_TRG_OFF: Capture of next image is automatically started after
+    #   previous.
+    #
+    # XI_TRG_LEVEL_HIGH: Specifies that the trigger is considered
+    #   valid as long as the level of the source signal is high.
+    #
+    # XI_TRG_LEVEL_LOW: Specifies that the trigger is considered valid
+    #   as long as the level of the source signal is low.
+
+
+@enum.unique
+class TrgSelectorMap(enum.Enum):
+    # The complete list is the XI_TRG_SELECTOR enum (C code) or in the
+    # xidefs module (Python code).
+
+    # Trigger starts the capture of one frame.
+    XI_TRG_SEL_FRAME_START = devices.TriggerMode.ONCE
+
+    # There are other modes/selector which look like they have matches
+    # on TriggerMode but we never got to test them:
+    #
+    # XI_TRG_SEL_EXPOSURE_ACTIVE: Trigger controls the start and
+    #   length of the exposure.
+    #
+    # XI_TRG_SEL_FRAME_BURST_START: Trigger starts the capture of the
+    #   bursts of frames in an acquisition.
+    #
+    # XI_TRG_SEL_FRAME_BURST_ACTIVE: Trigger controls the duration of
+    #   the capture of the bursts of frames in an acquisition.
+    #
+    # XI_TRG_SEL_MULTIPLE_EXPOSURES: Trigger which when first trigger
+    #   starts exposure and consequent pulses are gating
+    #   exposure(active HI)
+    #
+    # XI_TRG_SEL_EXPOSURE_START: Trigger controls the start of the
+    #   exposure of one Frame.
+    #
+    # XI_TRG_SEL_MULTI_SLOPE_PHASE_CHANGE: Trigger controls the multi
+    #   slope phase in one Frame (phase0 -> phase1) or (phase1 ->
+    #   phase2).
+    #
+    # XI_TRG_SEL_ACQUISITION_START: Trigger starts acquisition of
+    #   first frame.
+
+
 class XimeaCamera(devices.CameraDevice):
     """Ximea cameras
 
@@ -54,24 +149,44 @@ class XimeaCamera(devices.CameraDevice):
         self._acquiring = False
         self._exposure_time = 0.1
         self._handle = xiapi.Camera()
+        self._img = xiapi.Image()
         self._serial_number = serial_number
         self._roi = devices.ROI(None,None,None,None)
+
+        # When using the Settings system, enums are not really enums
+        # and even when using lists we get indices sent back and forth
+        # (works fine only when using EnumInt.  The gymnastic here
+        # makes it work with the rest of enums which are there to make
+        # it work with TriggerTypeMixIn.
+        trg_source_names = [x.name for x in TrgSourceMap]
+        def _trigger_source_setter(index: int) -> None:
+            trigger_mode = TrgSourceMap[trg_source_names[index]].value
+            self.set_trigger(trigger_mode, self.trigger_mode)
+        self.add_setting('trigger source', 'enum',
+                         lambda: TrgSourceMap(self.trigger_type).name,
+                         _trigger_source_setter,
+                         trg_source_names)
 
     def _fetch_data(self) -> typing.Optional[np.ndarray]:
         if not self._acquiring:
             return None
 
         try:
-            self._handle.get_image(self.img, timeout=1)
+            self._handle.get_image(self._img, timeout=1)
         except Exception as err:
             # err.status may not exist so use getattr (see
             # https://github.com/python-microscope/vendor-issues/issues/2)
             if getattr(err, 'status', None) == 10: # Timeout
                 return None
+            elif (getattr(err, 'status', None) == 45 # Acquisition is stopped
+                  and not self._acquiring):
+                # We can end up here during disable if self._acquiring
+                # was True but is now False.
+                return None
             else:
                 raise err
 
-        data = self.img.get_image_data_numpy() # type: np.ndarray
+        data = self._img.get_image_data_numpy() # type: np.ndarray
         _logger.info("Fetched imaged with dims %s and size %s.",
                      data.shape, data.size)
         return data
@@ -79,8 +194,15 @@ class XimeaCamera(devices.CameraDevice):
     def abort(self):
         _logger.info('Disabling acquisition.')
         if self._acquiring:
-            self._handle.stop_acquisition()
+            # We set acquiring before calling stop_acquisition because
+            # the fetch loop is still running and will raise errors 45
+            # otherwise.
             self._acquiring = False
+            try:
+                self._handle.stop_acquisition()
+            except:
+                self._acquiring = True
+                raise
 
     def initialize(self) -> None:
         """Initialise the camera.
@@ -117,19 +239,8 @@ class XimeaCamera(devices.CameraDevice):
                 raise Exception('failed to get DevId for device with SN %s'
                                 % self._serial_number)
 
-        # Try set camera into rising-edge hardware trigger mode.  If
-        # that can't be done set it to software trigger mode.
-        # TODO: even if the trigger source is set to edge rising, the
-        # camera can still be triggered by software.  For now, this
-        # lets us work with hardware and software triggers without
-        # having a setting for that (but we will need to change this
-        # one day). See issue #131.
-        try:
-            self._handle.set_trigger_source('XI_TRG_EDGE_RISING')
-        except:
-            self._handle.set_trigger_source('XI_TRG_SOFTWARE')
-        # create img buffer to hold images.
-        self.img = xiapi.Image()
+        self.set_trigger(devices.TriggerType.SOFTWARE,
+                         devices.TriggerMode.ONCE)
 
     def make_safe(self):
         if self._acquiring:
@@ -142,9 +253,9 @@ class XimeaCamera(devices.CameraDevice):
         _logger.info("Preparing for acquisition.")
         if self._acquiring:
             self.abort()
-        self._acquiring = True
         # actually start camera
         self._handle.start_acquisition()
+        self._acquiring = True
         _logger.info("Acquisition enabled.")
         return True
 
@@ -165,57 +276,17 @@ class XimeaCamera(devices.CameraDevice):
     def _get_sensor_shape(self):
         return (self._handle.get_width(), self._handle.get_height())
 
-    def get_trigger_source(self):
-        return (self._handle.get_trigger_source())
-
-    def get_trigger_type(self):
-        trig = self._handle.get_trigger_source()
-        _logger.info("called get trigger type %s", trig)
-        if trig == 'XI_TRG_SOFTWARE':
-            return devices.TRIGGER_SOFT
-        elif trig == 'XI_TRG_EDGE_RISING':
-            return devices.TRIGGER_BEFORE
-
-    def set_trigger_source(self, trig):
-        _logger.info("Set trigger source %s", trig)
-        reenable = False
-        if self._acquiring:
-            self.abort()
-            reenable = True
-        result = self._handle.set_trigger_source(trig)
-        _logger.info("Set trigger source result  %s", result)
-        if reenable:
-            self._on_enable()
-        return
-
-    def set_trigger_type(self, trig):
-        _logger.info("Set trigger type %s", trig)
-        self.abort()
-
-        if trig is 0:
-            self._handle.set_trigger_source('XI_TRG_SOFTWARE')
-        elif trig is 1:
-            self._handle.set_trigger_source('XI_TRG_EDGE_RISING')
-            # define digial input mode of trigger
-            self._handle.set_gpi_selector('XI_GPI_PORT1')
-            self._handle.set_gpi_mode('XI_GPI_TRIGGER')
-            self._handle.set_gpo_selector('XI_GPO_PORT1')
-            self._handle.set_gpo_mode('XI_GPO_EXPOSURE_ACTIVE')
-
-        self._on_enable()
-
-        result = self._handle.get_trigger_source()
-        _logger.info("Trigger type %s", result)
-        _logger.info("GPI Selector %s", self._handle.get_gpi_selector())
-        _logger.info("GPI Mode %s", self._handle.get_gpi_mode())
-
-        return
-
-    def soft_trigger(self):
-        _logger.info('Soft trigger received; self._acquiring is %s.',
-                     self._acquiring)
-        if self._acquiring:
-            self._handle.set_trigger_software(True)
+    def soft_trigger(self) -> None:
+        # We need to check this ourselves because, despite what the
+        # documentation says, the camera will accept software triggers
+        # even if its trigger source is not set for software.  See
+        # https://github.com/python-microscope/vendor-issues/issues/3
+        if self.trigger_type is not devices.TriggerType.SOFTWARE:
+            _logger.warning('ignoring soft_trigger() because camera is not'
+                            ' in software trigger type')
+        else:
+            _logger.debug('sending software trigger')
+            self._handle.set_trigger_software(1)
 
     def _get_binning(self):
         return (1, 1)
@@ -242,3 +313,38 @@ class XimeaCamera(devices.CameraDevice):
             self._handle.close_device()
         else:
             _logger.warning('shutdown() called but camera was already closed')
+
+    @property
+    def trigger_mode(self) -> devices.TriggerMode:
+        trg_selector = self._handle.get_trigger_selector()
+        try:
+            tmode = TrgSelectorMap[trg_selector]
+        except KeyError:
+            raise Exception('somehow set to unsupported trigger mode %s'
+                            % trg_selector)
+        return tmode.value
+
+    @property
+    def trigger_type(self) -> devices.TriggerType:
+        trg_source = self._handle.get_trigger_source()
+        try:
+            ttype = TrgSourceMap[trg_source]
+        except KeyError:
+            raise Exception('somehow set to unsupported trigger type %s'
+                            % trg_source)
+        return ttype.value
+
+    def set_trigger(self, ttype: devices.TriggerType,
+                    tmode: devices.TriggerMode) -> None:
+        if tmode is not devices.TriggerMode.ONCE:
+            raise Exception('%s not supported (only TriggerMode.ONCE)' % tmode)
+
+        try:
+            trg_source = TrgSourceMap(ttype)
+        except ValueError:
+            raise Exception('no support for trigger type %s' % ttype)
+
+        if trg_source.name != self._handle.get_trigger_source():
+            # Changing trigger source requires stopping acquisition.
+            with _disabled_camera(self):
+                self._handle.set_trigger_source(trg_source.name)
