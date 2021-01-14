54b3f8120340c792d03c390126f7b6085c24e6ea
David Pinto <david.pinto@bioch.ox.ac.uk>
2021-01-14 20:39:07 +0000

    maint: set version to 0.6.0+dev after 0.6.0 release.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools.command.sdist
 
 
 project_name = "microscope"
-project_version = "0.6.0"
+project_version = "0.6.0+dev"
 
 
 # setup.py is used for both maintainers actions (build documentation,
