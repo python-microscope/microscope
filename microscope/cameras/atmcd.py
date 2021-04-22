fea16e074bcf38da5abae05509c189c31006d694
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2021-04-22 15:48:46 +0100

    fix for andor EMCCD timing issue #203

--- microscope/cameras/atmcd.py
+++ microscope/cameras/atmcd.py
@@ -1718,7 +1718,12 @@ class AndorAtmcd(
         with self:
             exposure, accumulate, kinetic = GetAcquisitionTimings()
             readout = GetReadOutTime()
-        return exposure + readout
+            #IMD 20210422 DeepSIM timing is wrong as the keepclear cycles are
+            # not accounted for. 
+            #return exposure + readout
+            #This appears to allow the correct time between trigger pulses.
+            return kinetic
+
 
     def _set_readout_mode(self, mode_index):
         """Configure channel, amplifier and VS-speed."""
