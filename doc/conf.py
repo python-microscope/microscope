d6df97e6a4970607d8ada07e2190143d8fd4cd0c
David Miguel Susano Pinto <david.pinto@bioch.ox.ac.uk>
2022-12-31 02:10:09 +0000

    maint: move patching for autodoc from setup.py to doc/conf.py.

--- doc/conf.py
+++ doc/conf.py
@@ -8,11 +8,72 @@
 ## notice and this notice are preserved.  This file is offered as-is,
 ## without any warranty.
 
+import ctypes
 import datetime
 import sys
+import unittest.mock
 
 
 sys.path.insert(0, "../microscope")
+
+
+# autodoc imports the modules to be documented.  Modules that wrap the
+# device C libraries will load that library.  This would require all
+# device libraries to be available to build the documentation.
+# autodoc can mocks different modules (see `autodoc_mock_imports`) but
+# that's not enough for all of our cases.
+
+
+def patch_cdll():
+    real_c_dll = ctypes.CDLL
+
+    mocked_c_libs = [
+        # Andor's SDK for (EM)CCD cameras.  Loading of this libary
+        # should be in a separate microcope._wrappers.atmcd module and
+        # then autodoc could just mock it.
+        "atmcd32d",
+        "atmcd32d.so",
+        "atmcd64d",
+        "atmcd64d.so",
+        # pvcam's SDK.  Loading of this shared library should be in a
+        # separate microcope._wrappers.pvcam module and then autodoc
+        # could just mock it.
+        "pvcam.so",
+        "pvcam32",
+        "pvcam64",
+    ]
+
+    def cdll_diversion(name, *args, **kwargs):
+        if name in mocked_c_libs:
+            return unittest.mock.MagicMock()
+        else:
+            return real_c_dll(name, *args, **kwargs)
+
+    ctypes.WinDLL = cdll_diversion
+    ctypes.CDLL = cdll_diversion
+
+
+patch_cdll()
+
+
+def patch_sizeof():
+    # autodoc is mocking microscope._wrappers.dcamapi4 but we create
+    # one of these structs for a singleton.
+    real_sizeof = ctypes.sizeof
+
+    def sizeof_diversion(struct):
+        if (
+            hasattr(struct, "___sphinx_mock__")
+            and str(struct) == "microscope._wrappers.dcamapi4.API_INIT"
+        ):
+            return 40  # doesn't really matter
+        else:
+            return real_sizeof(struct)
+
+    ctypes.sizeof = sizeof_diversion
+
+
+patch_sizeof()
 
 
 # This should ve read from setup.py.  Maybe we should use
